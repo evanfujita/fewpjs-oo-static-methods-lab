d10e4a2f073d901bbafbe2ca991bb54c020668d1
Evan <evanfujita@gmail.com>
2021-02-22 20:22:57 -0600

    commit tree 811282cb5350d4380c67db6be1cecf5c42cebe64

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    let a = string[0]
+    let x = string[0].toUpperCase()
+    return string.replace(a, x)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+
 }
